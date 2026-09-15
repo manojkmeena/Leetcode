85b68ab22e40d5124bd931c6ebc3c7ed01899718
manojkmeena <36967136+manojkmeena@users.noreply.github.com>
2026-09-15 18:27:06 +0530

    Time: 14 ms (69.9%), Space: 51.7 MB (5.09%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,28 +1,26 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // Frequency Map
         HashMap<Integer, Integer> freqMap = new HashMap<>();
-        for(int n : nums){
-            freqMap.put(n, freqMap.getOrDefault(n, 0) + 1);
+        for(int num : nums){
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
 
-        // array with freq as index
-        List<Integer>[] freqArray = new List[nums.length + 1];
-        for (int i = 0; i < freqArray.length; i++) {
-            freqArray[i] = new ArrayList<>();
+        List<List<Integer>> freqArray = new ArrayList<>();
+        for (int i = 0; i <= nums.length; i++) {
+            freqArray.add(new ArrayList<>());
         }
-        freqMap.forEach((key, value) -> freqArray[value].add(key));
+        freqMap.forEach((value,freq) -> {
+            freqArray.get(freq).add(value);
+        });
 
-        // traverse freq array from back and return
         int[] res = new int[k];
-        int index = 0;
-        for(int i = freqArray.length -1; i>=0; i--){
-            for(int value: freqArray[i]){
-                res[index] = value;
-                index ++;
-                if(index == k){
+        int idx = 0;
+        for(int i = freqArray.size() - 1; i >=0; i--){
+            for(int num: freqArray.get(i)){
+                res[idx] = num;
+                idx++;
+                if(idx == k)
                     return res;
-                }
             }
         }
         return res;
