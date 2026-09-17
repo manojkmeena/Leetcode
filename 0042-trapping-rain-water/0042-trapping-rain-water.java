a402f288f5699dc1781d25ed25482f06e2a087f8
manojkmeena <36967136+manojkmeena@users.noreply.github.com>
2026-09-17 19:24:41 +0530

    Time: 0 ms (100%), Space: 47 MB (99.09%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -3,20 +3,18 @@ class Solution {
         int totalUnit = 0;
         int leftp = 0;
         int rightp = height.length-1;
-        int leftMax = 0;
-        int rightMax = 0;
-        while(leftp <= rightp){
+        int leftMax = height[leftp];
+        int rightMax = height[rightp];
+        while(leftp < rightp){
             if(leftMax < rightMax){
-                int unit = Math.min(leftMax,rightMax) - height[leftp];
-                totalUnit = unit > 0 ? totalUnit + unit : totalUnit;
-                leftMax = Math.max(leftMax, height[leftp]);
                 leftp++;
+                leftMax = Math.max(leftMax, height[leftp]);
+                totalUnit += leftMax - height[leftp];
             }
             else{
-                int unit = Math.min(leftMax,rightMax) - height[rightp];
-                totalUnit = unit > 0 ? totalUnit + unit : totalUnit;
-                rightMax = Math.max(rightMax, height[rightp]);
                 rightp--;
+                rightMax = Math.max(rightMax, height[rightp]);
+                totalUnit += rightMax - height[rightp];
             }
         }
         return totalUnit;
