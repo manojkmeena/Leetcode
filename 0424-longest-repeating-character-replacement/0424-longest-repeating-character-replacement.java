ffc2fdabcb931f45d8f908d1ca85191e12f1790e
manojkmeena <36967136+manojkmeena@users.noreply.github.com>
2026-09-24 18:51:08 +0530

    Time: 19 ms (33.25%), Space: 45.9 MB (95.19%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,25 +1,24 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         int l = 0;
-        int windowsize = 0;
-        int[] charcount = new int[26];
+        int[] windowCharcount = new int[26];
         int maxRepeating = 0;
         for (int r = 0; r < s.length(); r++) {
-            windowsize++;
-            charcount[s.charAt(r) - 'A'] += 1;
-            while (windowsize - getMaxFreq(charcount) > k) {
-                windowsize--;
-                charcount[s.charAt(l) - 'A'] -= 1;
+            int windowsize = r - l + 1;
+            windowCharcount[s.charAt(r) - 'A'] += 1;
+            while (windowsize - getMaxFreq(windowCharcount) > k) {
+                windowCharcount[s.charAt(l) - 'A'] -= 1;
                 l++;
+                windowsize--;
             }
             maxRepeating = Math.max(maxRepeating, windowsize);
         }
         return maxRepeating;
     }
 
-    private int getMaxFreq(int[] charcount) {
+    private int getMaxFreq(int[] windowCharcount) {
         int maxFreq = 0;
-        for (int freq : charcount) {
+        for (int freq : windowCharcount) {
             maxFreq = Math.max(maxFreq, freq);
         }
         return maxFreq;
