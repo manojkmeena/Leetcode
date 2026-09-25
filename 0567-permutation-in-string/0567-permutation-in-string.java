5255a2f21a12ec3897a24a9366744e2b07572c5b
manojkmeena <36967136+manojkmeena@users.noreply.github.com>
2026-09-25 13:09:12 +0530

    Time: 9 ms (38.33%), Space: 44.2 MB (48.49%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length() > s2.length()){
+        if (s1.length() > s2.length()) {
             return false;
         }
         int[] s1CharFreq = new int[26];
@@ -9,18 +9,35 @@ class Solution {
             s1CharFreq[s1.charAt(i) - 'a'] += 1;
             s2WindowCharFreq[s2.charAt(i) - 'a'] += 1;
         }
+        int matches = 0;
+        for (int i = 0; i < 26; i++) {
+            if (s1CharFreq[i] == s2WindowCharFreq[i]) {
+                matches++;
+            }
+        }
         int l = 0;
         int r = s1.length();
         while (r < s2.length()) {
-            if (Arrays.equals(s1CharFreq, s2WindowCharFreq)) {
+            if (matches == 26) {
                 return true;
-            } else {
-                s2WindowCharFreq[s2.charAt(l) - 'a'] -= 1;
-                s2WindowCharFreq[s2.charAt(r) - 'a'] += 1;
-                l++;
-                r++;
             }
+            int rIndex = s2.charAt(r) - 'a';
+            s2WindowCharFreq[rIndex] += 1;
+            if (s1CharFreq[rIndex] == s2WindowCharFreq[rIndex]) {
+                matches++;
+            } else if (s1CharFreq[rIndex] + 1 == s2WindowCharFreq[rIndex]) {
+                matches--;
+            }
+            int lIndex = s2.charAt(l) - 'a';
+            s2WindowCharFreq[lIndex] -= 1;
+            if (s1CharFreq[lIndex] == s2WindowCharFreq[lIndex]) {
+                matches++;
+            } else if (s1CharFreq[lIndex] - 1 == s2WindowCharFreq[lIndex]) {
+                matches--;
+            }
+            r++;
+            l++;
         }
-        return Arrays.equals(s1CharFreq, s2WindowCharFreq);
+        return matches == 26;
     }
 }
