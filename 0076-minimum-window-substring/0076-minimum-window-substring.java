dfe1ab1ad4b400df875b61cd3c57ec45328051f9
manojkmeena <36967136+manojkmeena@users.noreply.github.com>
2026-09-25 20:06:47 +0530

    Time: 20 ms (37.45%), Space: 47.9 MB (13.99%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -8,7 +8,7 @@ class Solution {
         int have = 0;
         int need = tCount.size();
         int minLength = Integer.MAX_VALUE;
-        int[] res = { -1, -1 };
+        int start = 0;
         int l = 0;
         for (int r = 0; r < s.length(); r++) {
             char c = s.charAt(r);
@@ -19,7 +19,7 @@ class Solution {
             while (have == need) {
                 if (minLength > r - l + 1) {
                     minLength = r - l + 1;
-                    res = new int[] { l, r };
+                    start = l;
                 }
                 char lc = s.charAt(l);
                 sWindowCount.put(lc, sWindowCount.get(lc) - 1);
@@ -29,6 +29,6 @@ class Solution {
                 l++;
             }
         }
-        return minLength == Integer.MAX_VALUE ? "" : s.substring(res[0], res[1] + 1);
+        return minLength == Integer.MAX_VALUE ? "" : s.substring(start, start + minLength);
     }
 }
